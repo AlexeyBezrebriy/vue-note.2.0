d4d25ad21ebcf1672274c119d9be5f54bc70ecc2
AlexeyBezrebriy <97750536+AlexeyBezrebriy@users.noreply.github.com>
2022-05-22 13:59:15 +0300

    deleted sort

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,6 @@
 <template>
   <li>
     <span v-bind:class="{ done: todo.completed }">
-      <input type="checkbox" v-on:change="todo.completed = !todo.completed" />
       <strong>{{ index + 1 }}</strong>
       {{ todo.title }}
     </span>
